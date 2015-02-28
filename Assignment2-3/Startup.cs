3371a0b62bdb27d343e235d57ed838676279632f
Gabriel Hung <ghung392@live.ca>
2015-02-28 10:31:53 -0800

    Created project

---
+++ Assignment2-3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment2_3.Startup))]
+namespace Assignment2_3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
