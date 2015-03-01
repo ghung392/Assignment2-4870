fb3c309c0a7a983c845ab36e9abc01e674c28e6b
Gabriel Hung <ghung392@live.ca>
2015-03-01 10:58:20 -0800

    Started entering lookup table entities

---
+++ Assignment2-3/Models/Entities/Client.cs
@@ -0,0 +1,27 @@
+﻿using Assignment2_3.Models.Lookup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_3.Models.Entities
+{
+    class Client
+    {
+        public int ClientReferenceNumber { get; set; }
+        public FiscalYear FiscalYear { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public string Surname { get; set; }
+        public string FirstName { get; set; }
+        public string PoliceFileNumber { get; set; }
+        public int CourtFileNumber { get; set; }
+        public int SwcFileNumber { get; set; }
+        public RiskLevel RiskLevel { get; set; }
+        public Service Service { get; set; }
+        public Program Program { get; set; }
+        public string RiskAssessAssignment { get; set; }
+        public RiskStatus RiskStatus { get; set; }
+    }
+}
---
+++ Assignment2-3/Models/Lookup/FiscalYear.cs
@@ -0,0 +1,19 @@
+﻿using Assignment2_3.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_3.Models.Lookup
+{
+    class FiscalYear
+    {
+        [Key]
+        public int FiscalYearId { get; set; }
+        public string Year { get; set; }
+
+        public ICollection<Client> Clients { get; set; }
+    }
+}
