6ee9dd592a91aedecd599ff9d76c6fecb9ce835e
Gabriel Hung <ghung392@live.ca>
2015-03-02 13:26:40 -0800

    Added clientcontext

---
+++ Assignment2-3/Models/Entities/ClientContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_3.Models.Entities
+{
+    class ClientContext : DbContext
+    {
+        public ClientContext() : base("DefaultConnection") { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Smart> Smarts { get; set; }
+    }
+}
