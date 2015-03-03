7c0b5d9b7412333caeabdf6d4de34e3240b2465e
Gabriel Hung <ghung392@live.ca>
2015-03-03 09:42:27 -0800

    completed client context

---
+++ Assignment2-3/Migrations/ClientMigrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Assignment2_3.Migrations.ClientMigrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Assignment2_3.Models.Entities.ClientContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\ClientMigrations";
+        }
+
+        protected override void Seed(Assignment2_3.Models.Entities.ClientContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- Assignment2-3/Models/Entities/ClientContext.cs
+++ Assignment2-3/Models/Entities/ClientContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assignment2_3.Models.Lookup;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -13,5 +14,40 @@ namespace Assignment2_3.Models.Entities
 
         public DbSet<Client> Clients { get; set; }
         public DbSet<Smart> Smarts { get; set; }
+        public DbSet<AbuserRelationship> AbuserRelationships { get; set; }
+        public DbSet<Age> Ages { get; set; }
+        public DbSet<AssignedWorker> AssignedWorkers { get; set; }
+        public DbSet<BadDateReport> BadDateReports { get; set; }
+        public DbSet<CityOfAssault> CityOfAssaults { get; set; }
+        public DbSet<CityOfResidence> CityOfResidences { get; set; }
+        public DbSet<Crisis> Crisises { get; set; }
+        public DbSet<DrugAssault> DrugAssaults { get; set; }
+        public DbSet<DuplicateFile> DuplicateFiles { get; set; }
+        public DbSet<Ethnicity> Ethnicities { get; set; }
+        public DbSet<EvidenceStored> EvidencesStored { get; set; }
+        public DbSet<FamilyViolenceFile> FamilyViolencesFiles { get; set; }
+        public DbSet<FiscalYear> FiscalYears { get; set; }
+        public DbSet<HivMeds> HivMeds { get; set; }
+        public DbSet<HospitalAttended> HospitalsAttended { get; set; }
+        public DbSet<Incident> Incidents { get; set; }
+        public DbSet<MedicalOnly> MedicalsOnly { get; set; }
+        public DbSet<MultiplePerpetrators> MultiplePerpetrators { get; set; }
+        public DbSet<PoliceAttendance> PoliceAttendances { get; set; }
+        public DbSet<PoliceReported> PolicesReported { get; set; }
+        public DbSet<Program> Programs { get; set; }
+        public DbSet<ReferralContact> ReferralContacts { get; set; }
+        public DbSet<ReferralSource> ReferralSources { get; set; }
+        public DbSet<ReferredCbvs> ReferredCbvses { get; set; }
+        public DbSet<ReferringHospital> ReferringHospitals { get; set; }
+        public DbSet<RepeatClient> RepeatClients { get; set; }
+        public DbSet<RiskLevel> RiskLevels { get; set; }
+        public DbSet<RiskStatus> RiskStatuses { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<SexWork> SexWorks { get; set; }
+        public DbSet<SocialWorkAttendance> SocialWorkAttendances { get; set; }
+        public DbSet<StatusOfFile> StatusOfFiles { get; set; }
+        public DbSet<ThirdPartyReport> ThirdPartyReports { get; set; }
+        public DbSet<VictimOfIncident> VictimsOfIncident { get; set; }
+        public DbSet<VictimServicesAttendance> VictimServicesAttendances { get; set; }
     }
 }
